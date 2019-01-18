c364aa36298cb1427896c5e8539a68c88ea21127
redfoxgamedev <devryzhov@gmail.com>
2019-01-17 23:34:17 -0800

    Add movement
    Add ability to move with trackpad, add teleportation

---
+++ Assets/Scripts/ArcTeleport.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArcTeleport : MonoBehaviour
+{
+    public Transform controller;
+    public Transform player;
+
+    public Transform objectDebug;
+
+    private bool isTeleportBegin;
+    private Vector3 teleportTarget;
+
+    public LineRenderer visualArc;
+
+    private int lineSegments = 20;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (OVRInput.Get(OVRInput.Button.PrimaryIndexTrigger) || Input.GetKey(KeyCode.W)){
+            //begin teleport
+            isTeleportBegin = true;
+            visualArc.enabled = true;
+            //teleport planning action
+            planTeleport();
+        }
+
+        if (OVRInput.GetUp(OVRInput.Button.PrimaryIndexTrigger) && isTeleportBegin){
+            //teleport! (if it began)
+            isTeleportBegin = false;
+            visualArc.enabled = false;
+
+            //teleport action
+            player.transform.position = teleportTarget + Vector3.up * 1.7f;
+        }
+    }
+
+    private void planTeleport()
+    {
+       
+        //Vector3 localPos = OVRInput.GetLocalControllerPosition(OVRInput.Controller.Active);
+        //Debug.Log(localPos);
+        //objectDebug.position = controller.position+controller.forward*1.5f;
+        Vector3 beginPoint = controller.position;
+       // Debug.Log("Controller pos " + beginPoint);
+        Vector3 endPoint = beginPoint + controller.forward * 20 - controller.up * 5;
+        Vector3 controlPoint = Vector3.Lerp(beginPoint, endPoint, 0.5f) + controller.up * 10;
+
+        float sampleStep = 1.0f / (lineSegments-1);
+        float currentStep = 0f;
+
+        Vector3[] samplePoints = new Vector3[lineSegments];
+
+        for (int i = 0; i < lineSegments; i++){
+            samplePoints[i] = sampleCurve(beginPoint, endPoint, controlPoint, currentStep);
+            currentStep += sampleStep;
+            //Debug.Log("Step " + i);
+        }
+        visualArc.positionCount = lineSegments;
+        visualArc.SetPositions(samplePoints);
+
+        RaycastHit hitInfo;
+        for (int i = 0; i < lineSegments-1; i++){
+            Vector3 pointBegin = samplePoints[i];
+            Vector3 pointEnd = samplePoints[i + 1];
+
+            if (Physics.Linecast(pointBegin, pointEnd, out hitInfo)){
+                Vector3 hitPosition = hitInfo.point;
+                objectDebug.position = hitPosition;
+                teleportTarget = hitPosition;
+                break;
+            }
+        }
+
+    }
+
+    Vector3 sampleCurve(Vector3 start, Vector3 end, Vector3 control, float t){
+        // Interpolate along line S0: control - start;
+        Vector3 Q0 = Vector3.Lerp(start, control, t);
+        // Interpolate along line S1: S1 = end - control;
+        Vector3 Q1 = Vector3.Lerp(control, end, t);
+        // Interpolate along line S2: Q1 - Q0
+        Vector3 Q2 = Vector3.Lerp(Q0, Q1, t);
+        return Q2; // Q2 is a point on the curve at time t
+    }
+}
---
+++ Assets/Scripts/PadMovement.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PadMovement : MonoBehaviour {
+
+    public static string TAG = "VR: ";
+
+    public Transform playerEyes;
+    public Transform player;
+    public Renderer debugObject;
+
+    
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if (OVRInput.Get(OVRInput.Touch.PrimaryTouchpad) || (Input.GetKey(KeyCode.R))){
+            //debugObject.enabled = true;
+            Vector2 padDirection = OVRInput.Get(OVRInput.Axis2D.PrimaryTouchpad); //already normalized
+            //padDirection = Vector2.up;
+
+            if (padDirection.y >=0.5f){
+                //general moving forward
+                Vector3 currentDirection = Vector3.ProjectOnPlane(playerEyes.transform.forward, Vector3.up).normalized;
+                player.transform.Translate(currentDirection * 1 * Time.deltaTime, Space.World);
+            }
+
+
+            //Vector2 padForward = Vector2.up; //normalized
+
+            //Vector2 padDifference = padForward - padDirection; //not normalized?
+
+            //Debug.Log(TAG + "Pad direction is "+padDirection);
+
+            //Vector3 currentDirection = Vector3.ProjectOnPlane(playerEyes.transform.forward, Vector3.up);
+
+            //Debug.DrawRay(playerEyes.transform.position, currentDirection, Color.blue);
+            
+
+            //Debug.Log(TAG + "Current player direction is " + currentDirection);
+            //Vector3 padDifferenceWorld = new Vector3(padDifference.x, 0, padDifference.y);
+
+            //Debug.Log(TAG + "World pad direction is " + padDifferenceWorld);
+
+            //Vector3 resultMovementDirection = (currentDirection + padDifferenceWorld).normalized;
+
+            //Debug.DrawRay(playerEyes.transform.position, resultMovementDirection, Color.green);
+
+            //Debug.Log(TAG + "Result movement is " + resultMovementDirection);
+            
+            //player.transform.Translate(resultMovementDirection * 1 * Time.deltaTime, Space.World);
+
+        }
+
+        if (OVRInput.GetUp(OVRInput.Touch.PrimaryTouchpad)){
+            debugObject.enabled = false;
+        }
+
+	}
+}
