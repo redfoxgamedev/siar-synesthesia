dcb88b41017fc88fddadf2237ffc30b3ad5914d3
Nicholas Ramsay <nicholasramsay@hotmail.com>
2019-01-21 13:03:57 -0800

    Added in door greybox w/animation

---
+++ Assets/Scripts/coroutineTimer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class coroutineTimer : MonoBehaviour {
+
+    public Animation anim;
+
+    void Start()
+    {
+        StartCoroutine(Example());
+
+        anim = gameObject.GetComponent<Animation>();
+      
+    }
+
+    IEnumerator Example()
+    {
+        print(Time.time);
+        yield return new WaitForSeconds(3);
+        print("Bloaw");
+        anim.Play("dooropen");
+    }
+}
